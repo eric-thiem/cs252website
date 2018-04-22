30c34bd4579a1fdef9e8ff9f39a7cd12eb10120f
Eric Thiem <ericthiem23@gmail.com>
2018-04-22 13:12:03 -0700

    Users can now create new accounts

--- src/SignIn/CreateAccount.js
+++ src/SignIn/CreateAccount.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { fireauth, firestore } from '../base.js';
-import { Form, FormGroup, Label, Input, Button, Alert, Container, Row, Col } from 'reactstrap';
+import { Form, Input, Button, Alert, Row, Col } from 'reactstrap';
 import './SignIn.css'
 
 class CreateAccount extends Component {
@@ -10,15 +10,84 @@ class CreateAccount extends Component {
     super(props);
 
     this.state = {
-      uid: props.uid,
+      username: null,
+      email: null,
+
       error_message: '',
-      error_visible: '',
+      error_visible: false,
     }
   }
 
-  onSubmit = (ev) => {
+  onFormSubmit = (ev) => {
+    ev.preventDefault();
+    let username = ev.target.username.value;
+    let email = ev.target.email.value;
+    let password = ev.target.password.value;
+    let confirm_password = ev.target.confirm_password.value;
 
+    if (username === '') {
+      this.setError('Please enter a valid username.')
 
+    } else if (email === '' || !email.includes('@') || !email.includes('.')){
+      this.setError('Pleaes enter a valid email.');
+
+    } else if (password === ''){
+      this.setError('Please enter a valid password.');
+
+    } else if (confirm_password === ''){
+      this.setError('Please confirm your password.')
+
+    } else if (password !== confirm_password){
+      this.setError('Passwords do not match.');
+    }
+
+    this.setState({
+      username: username,
+      email: email,
+    });
+
+    this.createAccount(email, password);
+  };
+
+  createAccount(email, password){
+    let self = this;
+    fireauth.createUserAndRetrieveDataWithEmailAndPassword(email, password).then(function () {
+      self.addUserInfo();
+    }).catch(function (error){
+      console.log(error);
+    });
+  };
+
+  addUserInfo(){
+    let self = this;
+    fireauth.onAuthStateChanged((user) => {
+        if (user) {
+          let docRef = firestore.collection("users").doc(user.uid);
+          docRef.set({
+            username: self.state.username,
+            email: self.state.email,
+          }).catch(function(error) {
+            console.log(error);
+          });
+        }/* else {
+          // finished signing out
+          self.setState({ uid: null })
+        }*/
+      }
+    );
+  };
+
+  setError(error){
+    this.setState({
+      error_message: error,
+      error_visible: true,
+    });
+  };
+
+  onDismiss = () => {
+    this.setState({
+      error_visible: false,
+    });
   };
 
   render() {
@@ -32,18 +101,26 @@ class CreateAccount extends Component {
 
             <div className='text-center'>
 
-              <Form className='verticalCenter' onSubmit={this.onSubmit}>
+              <Form className='verticalCenter' onSubmit={this.onFormSubmit}>
 
-                <h2>Welcome!</h2>
+                <h2>Create New Account!</h2>
 
                 <div style={{height: '1em'}}/>
 
                 <Input
                   type='text'
-                  id='email'
+                  id='username'
                   bsSize='lg'
-                  placeholder='Email'
+                  placeholder='Username'
                   style={{border: '1px solid #4682B4'}}/>
+
+                <div style={{height: '1em'}}/>
+
+                <Input type='text'
+                       id='email'
+                       bsSize='lg'
+                       placeholder='Email'
+                       style={{border: '1px solid #4682B4'}}/>
 
                 <div style={{height: '1em'}}/>
 
@@ -55,11 +132,23 @@ class CreateAccount extends Component {
 
                 <div style={{height: '1em'}}/>
 
-                <Button className='signInButton'> Sign In </Button>
+                <Input type='password'
+                       id='confirm_password'
+                       bsSize='lg'
+                       placeholder='Confirm Password'
+                       style={{border: '1px solid #4682B4'}}/>
 
-                <div style={{height: '1em'}}/>
+                <hr/>
 
-                <Button className='signInButton'> Create Account </Button>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+
+                  <Alert color="danger" isOpen={this.state.error_visible} toggle={this.onDismiss}>
+                    {this.state.error_message}
+                  </Alert>
+
+                  <Button className='signInButton'> Create Account </Button>
+
+                </div>
 
               </Form>
 
--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { fireauth } from '../base.js';
 
 import { NavLink } from 'react-router-dom';
-import { Form, Label, Input, Button, Alert, Row, Col} from 'reactstrap';
+import { Form, Input, Button, Alert, Row, Col} from 'reactstrap';
 import './SignIn.css';
 
 class SignIn extends Component {
@@ -43,7 +43,7 @@ class SignIn extends Component {
           <Col md='5'/>
           <Col md='2'>
 
-            <div>
+            <div className='text-center'>
 
               <Form className='verticalCenter' onSubmit={this.onSubmit}>
 
@@ -68,7 +68,7 @@ class SignIn extends Component {
                        className='input'
                        style={{border: '1px solid #4682B4'}}/>
 
-                <hr />
+                <hr/>
 
                 <NavLink style={{ textDecoration: 'none' }} to="/create-account">
                   <Button className='signInButton'> Sign In </Button>
@@ -82,7 +82,7 @@ class SignIn extends Component {
 
                 <div style={{height: '1em'}}/>
 
-                <NavLink style={{ textDecoration: 'none' }} to="/forgotPassword">
+                <NavLink style={{ textDecoration: 'none' }} to="/forgot-password">
                   <p style={{fontSize: '1.2em', color: '#0d39ed', cursor: 'pointer'}}><u>Forgot Password</u></p>
                 </NavLink>
 
