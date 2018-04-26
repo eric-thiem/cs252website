8f061e126a8b864f4d8b4dfbb8ef71adaa002f47
Eric Thiem <ericthiem23@gmail.com>
2018-04-25 18:06:04 -0700

    Users can now see their favorites and watchlist

--- src/Tabs/MyFavorites.js
+++ src/Tabs/MyFavorites.js
@@ -1,15 +1,84 @@
 import React, { Component } from 'react';
+import {Row, Col, Jumbotron} from 'reactstrap';
+import {firestore} from "../base";
 
-class MyFavorites extends Component {
+class Watchlist extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      doneLoading: false,
+    }
+  }
+
+  componentWillMount(){
+    this.getFavoritesList();
+  }
+
+  getFavoritesList(){
+    let self = this;
+    let myFavorites = [];
+    let myRef = firestore.collection('users').doc(sessionStorage.getItem('user'));
+    myRef.get().then(function (doc){
+      myFavorites = doc.data().favorites;
+      self.setState({
+        favorites: myFavorites,
+        doneLoading: true,
+      });
+    });
+  }
 
   render(){
+
+    if(!this.state.doneLoading){
+      return(
+        <div className='text-center'>
+          <h3>Loading Favorites...</h3>
+        </div>
+      );
+    }
+
     return (
-      <div className='text-center'>
-        <h3>My Favorites!</h3>
+      <div>
+        <Row>
+          <Col md={{size: '8', offset: '2'}}>
+
+            <div className='text-center'>
+              <h1>My Favorites</h1>
+            </div>
+
+            <div className='space'/>
+
+            {Object.keys(this.state.favorites).map((key, index) => {
+              return (
+
+                <Jumbotron>
+                  <Row>
+                    <Col md='4'>
+                      <img src={this.state.favorites[index].poster}/>
+                    </Col>
+
+                    <Col md='6'>
+
+                      <h1> {this.state.favorites[index].title} </h1>
+
+                      <h3> Rating: {this.state.favorites[index].rating} </h3>
+
+                      <a href={this.state.favorites[index].imdburl}> Visit IMDB Page </a>
+
+                    </Col>
+                  </Row>
+                </Jumbotron>
+
+              );
+            })}
+          </Col>
+        </Row>
       </div>
     );
   }
 
 }
 
-export default MyFavorites;
+export default Watchlist;
